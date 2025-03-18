95db40c790a410c539187cc6d7326bcd8d0cec6e
Francis Mule <francismule300@gmail.com>
2025-03-18 21:44:30 +0300

    Even or Odd Checker First Commit

---
+++ App.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Even or Odd Checker App");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+
+                try {
+                    System.out.println("1. Check Even or Odd");
+                    System.out.println("2. Exit");
+                    System.out.print("Enter your choice: ");
+                    int choice = scanner.nextInt();
+
+                    switch (choice) {
+                        case 1 -> {
+                            System.out.print("Enter a number: ");
+                            int number = scanner.nextInt();
+                            if (number % 2 == 0) {
+                                System.out.println(number + " is an even number");
+                            } else {
+                                System.out.println(number + " is an odd number");
+                            }
+                        }
+                        case 2 -> {
+                            System.out.println("Thank you for using the app");
+                            System.exit(0);
+                        }
+                    }
+                } catch (Exception e) {
+                    System.out.println("Invalid input. Please try again");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# Even or Odd Checker App
+
+This is a simple Java application that allows users to check whether a number is even or odd.
+
+## Features
+
+- Check if a number is even or odd
+- Simple and easy-to-use interface
+
+## How to Use
+
+1. Run the application.
+2. Choose the option to check if a number is even or odd.
+3. Enter the number you want to check.
+4. The application will display whether the number is even or odd.
+5. Choose the option to exit the application when you are done.
+
+## Compile and Run
+
+To compile and run the Even or Odd Checker App, follow these steps:
+
+1. Open a terminal or command prompt.
+2. Navigate to the directory where `App.java` is located.
+3. Compile the Java file using the following command:
+   ```sh
+   javac -d bin App.java
+   ```
+4. Run the compiled Java class using the following command:
+   ```sh
+   java -cp bin App
+   ```
